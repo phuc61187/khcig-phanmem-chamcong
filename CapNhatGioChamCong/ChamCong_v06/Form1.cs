25e8bd89521e15d5b6b1a4afa6b2d8d30d538e55
phuc61187 <phuc61187@gmail.com>
2015-08-13 08:03:14 +0700

    test sourcetree

--- CapNhatGioChamCong/ChamCong_v06/Form1.cs
+++ CapNhatGioChamCong/ChamCong_v06/Form1.cs
@@ -6,11 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ChamCong_v06.Helper;
 
 namespace ChamCong_v06 {
 	public partial class Form1 : Form {
 		public Form1() {
 			InitializeComponent();
+			ACMessageBox.Show("Test sourcetere", Resources.Caption_ThongBao, 2000);
 		}
 	}
 }
